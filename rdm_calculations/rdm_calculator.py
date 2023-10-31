3e16fed798dcafa22fc37ef52e82cb15fa42060a
idangrosbard <idangrosbard@mail.tau.ac.il>
2023-10-31 09:13:28 +0200

    Created base class for RDM calculators

---
+++ rdm_calculations/rdm_calculator.py
@@ -0,0 +1,20 @@
+
+
+
+class RDMCalculator(object):
+    """Base class for RDM calculators."""
+
+    def calc_rdm(model: nn.Module, preprocess: Callable[[Image.Image], Tensor], imgs_paths: List[str], layers_names: Dict[Any, str]) -> Dict[str, np.ndarray]:
+        """
+        Calculate RDMs for a given model and images.
+
+        Args:
+            model: the model used to get the representations.
+            preprocess: a function that preprocesses an image to a tensor (model compatible).
+            imgs_paths: a list of paths to images.
+            layers_names: a list of layers names to calculate RDMs for.
+
+        Returns:
+            A dictionary of RDMs for each layer.
+        """
+        raise NotImplementedError('RDM calculator is a base class and should not be instantiated.')
